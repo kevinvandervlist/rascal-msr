9efaf3024b8907fa0a0928a9cef0cb215b563e73
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2012-12-13 17:29:17 +0100

    Lex a file at a time instead of per line

--- src/advtrack/kevin/lexer/Lexer.java
+++ src/advtrack/kevin/lexer/Lexer.java
@@ -1,5 +1,10 @@
 package advtrack.kevin.lexer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.antlr.runtime.*;
 import org.eclipse.imp.pdb.facts.*;
 
@@ -16,6 +21,105 @@ public class Lexer {
 		return this.valueFactory.string(lexer(rawline.getValue()));
 	}
 	
+	public IValue lexFile(ISourceLocation sloc) {
+		CharStream cs = null;
+		List<String> list = null;
+		
+		try {
+			cs = new ANTLRFileStream(sloc.getURI().getPath());
+			
+			JavaLexer lexer = new JavaLexer(cs);
+			
+			list = tokenizeSourceFile(lexer);
+		} catch (IOException e) {
+			System.err.println(e.getStackTrace());
+		}
+		
+		if(list != null) {
+			// Create a list to return to rascal 
+			Iterator<String> it = list.iterator();
+			
+			IList retl = this.valueFactory.list();
+			retl.append(this.valueFactory.string("WTF"));
+
+			/*
+			for(int i = 0; i < list.size(); i++) {
+				retl.append(this.valueFactory.string(list.get(i)));
+			}
+			
+			for(int i = 0; i < 10; i++) {
+				retl.append(this.valueFactory.string(list.get(i)));
+			}
+			*/
+			
+			while(it.hasNext()) {
+				IString cur = this.valueFactory.string(it.next());
+				retl = retl.append(cur);
+			}
+			return retl;
+		} else {
+			return this.valueFactory.list();
+		}
+	}
+	
+	/**
+	 * Based on a given lexer, parse a source file in such a way that
+	 * all rewrite rules replace the needed tokens.
+	 * @param lexer
+	 * @return
+	 */
+	
+	private List<String> tokenizeSourceFile(JavaLexer lexer) {
+		CommonTokenStream tokens = new CommonTokenStream();
+		List<String> lines = new ArrayList<String>();
+		
+        StringBuffer buf = new StringBuffer();
+        int curline = 1;
+        int prevline = 1;
+
+		tokens.setTokenSource(lexer);
+		
+        Token token = lexer.nextToken();
+
+        // Create a list of lines, where tokens correspond to the original source file.
+        while(token.getType() != JavaLexer.EOF) {
+        	int cl = token.getLine();
+        	
+        	if (cl == (prevline + 1) || curline != cl) {
+        		lines.add(buf.toString());
+        		if (curline != cl) {
+        			int diff = cl - prevline - 1;
+        			for(int i = 0; i < diff; i++) {
+        				lines.add("");
+        			}
+        		}
+        		buf = new StringBuffer();
+        	}
+        	
+        	buf.append(applyRewriteRules(token));
+        	
+        	// Make sure to update the counters
+        	curline = cl;
+        	prevline = cl;
+        	
+        	token = lexer.nextToken();
+        }
+        
+        lines.add(buf.toString());
+
+        return lines;
+	}
+	
+	/**
+	 * Call the rewrit rules that are needed for the current token.
+	 * @param token
+	 * @return The string to rewrite the token to.
+	 */
+	
+	private String applyRewriteRules(Token token) {
+		return token.getText();
+	}
+
 	/**
 	 * This function takes a string from a Java source file, and then
 	 * lexes it according to the Java grammar.
--- src/advtrack/kevin/lexer/Lexer.rsc
+++ src/advtrack/kevin/lexer/Lexer.rsc
@@ -2,3 +2,6 @@ module advtrack::kevin::lexer::Lexer
 
 @javaClass{advtrack.kevin.lexer.Lexer}
 public java str lexLine(str rawline);
+
+@javaClass{advtrack.kevin.lexer.Lexer}
+public java list[str] lexFile(loc file);
